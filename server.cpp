b6e0a46472fd61fd498cfe64e0cd4e4ba661685e
root <root@Denchik.localdomain>
2025-08-28 11:37:55 +0200

    i made some minor coorrections

--- server.cpp
+++ server.cpp
@@ -20,7 +20,7 @@ struct sockaddr_in FillAddr(struct sockaddr_in ServAddr, const char *ip, int Ser
     if (!inet_aton(ip, &(ServAddr.sin_addr)))
 	{
 		printf("error with filling sin_addr: %d\n", errno);
-		ServAddr.sin_port = NULL;
+		ServAddr.sin_port = 0;
 		return ServAddr;
 	}else printf("sin_addr was filled\n");
 	return ServAddr;
@@ -118,7 +118,7 @@ int main()
     fd_set readfds, writefds, exceptfds;
 
     FillAddr(ServAddr, ip, ServPort);
-	if (ServAddr.sin_port == NULL)
+	if (ServAddr.sin_port == 0)
 	{
 		return -1;
 	}
