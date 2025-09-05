b65946a25e89f76c6d1c42c08e3709b4612f5bfa
root <root@Denchik.localdomain>
2025-09-05 16:23:52 +0200

    a made some minor corrections

--- typeless_type.h
+++ typeless_type.h
@@ -29,30 +29,6 @@ typedef struct typeless
         type = typ;
     }
 
-    typeless(const typeless& copy)
-    {
-        printf("sbjkfas\n");
-        Vector VectorValue;
-        int intValue;
-        value = new void*;
-        switch (copy.type)
-        {
-        case INT:
-            intValue = *static_cast<int*>(value);
-            printf("value: %d\n", intValue);
-            value = &intValue; 
-            break;
-        case VECTOR:
-            VectorValue = *static_cast<Vector*>(value);
-            printf("value:%d\n", VectorValue.x);
-            value = &VectorValue; 
-            break;    
-        default:
-            break;
-        }
-        type = copy.type;
-    }   
-
     void setValue(void *val, Type typ)      
     {
         value = val;
