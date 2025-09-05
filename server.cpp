77a30e5d38e2740f52ebea6ef4915c93d7ba3c13
root <root@Denchik.localdomain>
2025-09-05 16:44:00 +0200

    a made some minor corrections

--- server.cpp
+++ server.cpp
@@ -173,7 +173,7 @@ int main()
             if(FD_ISSET(pd[i], &readfds))
             {
 		    	printf("пришло сообщение от игрока\n");
-				if (0 > (ReadBytes = read(pd[i], &messangeFrom[i], sizeof(messangeFrom[i]))))
+				if (0 > (ReadBytes = read(pd[i], &messangeFrom[i], sizeof(&messangeFrom[i]))))
 				{
 					printf("ошибка чтения данных:%d\n", errno);
 					return -1;
