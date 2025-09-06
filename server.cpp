5c7373289171b266b8e9b672d8db6a637849b15a
root <root@Denchik.localdomain>
2025-09-06 11:56:09 +0200

    a made some minor corrections

--- server.cpp
+++ server.cpp
@@ -190,8 +190,9 @@ int main()
 					if(std::holds_alternative<int>(messangeFrom[i]))
 					{
 						key = std::get<int>(messangeFrom[i]);
+						printf("jopa1\n");
 						printf("сообщение от игрока:%d\n", key);
-						printf("ReadBytes\n");
+						printf("jopa2\n");
 						switch (key)
 						{
 						case up:
