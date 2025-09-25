daf7d85bf17f6891ef1a357ff33140b2bed7c811
root <root@DESKTOP-V03ARSE.localdomain>
2025-09-25 05:55:32 -0700

    i made some minor corrections

--- client.cpp
+++ client.cpp
@@ -58,7 +58,7 @@ void explode(int BombPositionY, int BombPositionX)
     float waitingTime = 0.5;
     for(int i = 0; i < AffectedArea; i++)
     {
-        while(stopwatch(waitingTime, time(NULL)) != false)
+        while(stopwatch(waitingTime, time(NULL)) == true)
         {
             for(int x = BombPositionX-i; x < BombPositionX+i; x++)
             {
