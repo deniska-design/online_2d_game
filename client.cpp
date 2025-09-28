1a03ccd1b94a2886b383d5ddb9fd765df694ee4f
root <root@DESKTOP-V03ARSE.localdomain>
2025-09-27 23:14:33 -0700

    i corrected a prog

--- client.cpp
+++ client.cpp
@@ -199,7 +199,7 @@ int main()
     {
         SetFdss(sd, readfds);
         FD_SET(STDIN_FILENO, &readfds);
-        timeout.tv_sec = 1;
+        timeout.tv_sec = 0.000001;
         if ((SelRes = select(MaxD+1, &readfds, NULL, NULL, &timeout)) == -1)
         {
             mvprintw(0, 0, "%d", SelRes);
