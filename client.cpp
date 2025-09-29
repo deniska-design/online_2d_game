49028d0e0d0171d65098dd0b9bec52da41f44a05
root <root@DESKTOP-V03ARSE.localdomain>
2025-09-29 08:52:47 -0700

    i made some minor corrections

--- client.cpp
+++ client.cpp
@@ -214,7 +214,7 @@ int main()
         {
             if(bombExploding)
             {
-                bombExploding = !explode(Bomb.GetY(), Bomb.GetX(), PositionBorders, 1, timeout.tv_sec);
+                bombExploding = !explode(Bomb.GetY(), Bomb.GetX(), PositionBorders, 1, 3);
             }
         }
 
