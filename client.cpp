b513624e8ba1d9165824525ea04dae35e11a5cf3
root <root@DESKTOP-V03ARSE.localdomain>
2025-10-05 02:58:38 -0700

    i made some minor corrections

--- client.cpp
+++ client.cpp
@@ -286,7 +286,7 @@ int main()
             Object.Hide();
             if (Object.getStatue() == active) 
             {
-                mvprintw(0, 0, "printing of player\n");
+                mvprintw(Object.GetY(), Object.GetX(), "printing of player\n");
                 Object.Show();
             }/*else if (Object.getType() == BombType)
             {
