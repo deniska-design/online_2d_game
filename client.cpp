e266e65552ecab4a05d6f55c98366985dd68d671
denys <root@DESKTOP-V03ARSE.localdomain>
2025-10-30 07:44:58 -0700

    i made some minor corrections

--- client.cpp
+++ client.cpp
@@ -166,10 +166,7 @@ int main()      //проблема с бомбами: игра прорисов�
     musicName[BombExplodingMusic] = "ahhh-for-donbass.wav";
     musicName[StartMusic] = "damn-why-did-i-come-here.wav";
     musicName[hitSound] = "ay-i-got-sniped-in-flight.wav";
-    sound HintergroundMus(musicName, 4);
-    sound BombExplodingMus(musicName, 4);
-    sound StartMus(musicName, 4);
-    sound hitSou(musicName, 4);
+    sound HintergroundMusic(musicName, 4);
     colors Color;
     struct timeval timeout;
     object MessangeFrom; 
@@ -204,7 +201,7 @@ int main()      //проблема с бомбами: игра прорисов�
 
     StartWindow();
     start_color();
-    HintergroundMus.playSound(musicName[HintergroundMusic]);
+    music.playSound(musicName[StartMusic]);
 
     init_pair(NothingColor, COLOR_BLACK, COLOR_BLACK);
     init_pair(PlayerColor, COLOR_YELLOW, COLOR_BLACK);
@@ -331,7 +328,7 @@ int main()      //проблема с бомбами: игра прорисов�
                 refresh(); 
             }else if (Object.getType() == BombType)
             {
-                BombExplodingMus.playSound(musicName[BombExplodingMusic]);
+                music.playSound(musicName[BombExplodingMusic]);
                 Bomb.setPosition(Object.GetY(), Object.GetX());
                 bombExploding = !explode(Bomb.GetY(), Bomb.GetX(), PositionBorders, 1, BombColor);  
                 refresh();
@@ -344,7 +341,7 @@ int main()      //проблема с бомбами: игра прорисов�
                             if(Player.GetY() < Bomb.GetY() + AffectedArea*AffectedAreaYCoefficient)
                             {
                                 Player.GetHP()--;
-                                hitSou.playSound(musicName[hitSound]);
+                                music.playSound(musicName[hitSound]);
                             }
                         }
                     }
@@ -373,9 +370,9 @@ int main()      //проблема с бомбами: игра прорисов�
             }
             MustSend = false;
         } 
-        if (!HintergroundMus.IsSoundPLaying())
+        if (!music.IsSoundPLaying())
         {
-            HintergroundMus.playSound(musicName[HintergroundMusic]);
+            music.playSound(musicName[HintergroundMusic]);
         }
         
         //конец
